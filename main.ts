fe2bdcb1847bcca97d3bea49cba7c7c2a27db8b4
tooba16 <165816363+tooba16@users.noreply.github.com>
2024-10-26 22:38:38 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log(`\n\t-----------------
+                                    WELCOME TO MY SIMPLE CALCULATOR
+                                                                        -----------------------------------\n`)
+
+const answer = await inquirer.prompt([
+  { message: "Enter your first number", type: "number", name: "firstNumber" },
+  { message: "Enter your second number", type: "number", name: "secondNumber" },
+  { message: "Select one of the operators to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+//conditional  ststement
+if(answer. operator === "Addition"){  
+     console.log( answer.firstNumber + answer.secondNumber);
+}else if (answer. operator === "Subtraction"){  
+    console.log( answer.firstNumber - answer.secondNumber);
+}else if(answer. operator === "Multiplication"){  
+    console.log( answer.firstNumber * answer.secondNumber);
+}else if (answer. operator === "Division"){  
+    console.log( answer.firstNumber / answer.secondNumber);
+}else{
+    console.log("Please select valid operator")
+}
+//********************************************** THE END ***************************************************/git
